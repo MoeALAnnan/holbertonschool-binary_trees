a617fa2255ac79078a2524cecb1ad544a794281f
MoeALAnnan <5653@holbertonstudents.com>
2023-09-15 17:12:10 +0000

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,12 +7,12 @@
  * Return: 1 if node is a leaf, otherwise 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
-{
-    if (node == NULL)
-        return (0); /* If the node is NULL, it cannot be a leaf */
-
-    if ((node->left == NULL) && (node->right == NULL))
-        return (1); /* If both left and right children are NULL, it is a leaf */
-
-    return (0); /* If it has at least one child, it is not a leaf */
+{   /* If the node is NULL, it cannot be a leaf */
+	if (node == NULL)
+		return (0);
+	/* If both left and right children are NULL, it is a leaf */
+	if ((node->left == NULL) && (node->right == NULL))
+		return (1);
+	/* If it has at least one child, it is not a leaf */
+	return (0);
 }
