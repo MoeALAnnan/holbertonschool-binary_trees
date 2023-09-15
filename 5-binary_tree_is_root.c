cdb66e2f63cb7b86d7559925d4aefc2c99431920
MoeALAnnan <5653@holbertonstudents.com>
2023-09-15 17:15:21 +0000

    betty fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,7 +9,7 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {   /* If the node has no parent, it is a root */
 	if (node && node->parent == NULL)
-		return (1); 
+		return (1);
 	/* If it has a parent or is NULL, it is not a root */
 	return (0);
 }
