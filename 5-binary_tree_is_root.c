06e047820b38041fbb152e1836546bbccefecbcd
MoeALAnnan <5653@holbertonstudents.com>
2023-09-15 17:13:20 +0000

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,9 +7,9 @@
  * Return: 1 if true, 0 if false
  */
 int binary_tree_is_root(const binary_tree_t *node)
-{
-    if (node && node->parent == NULL)
-        return (1); /* If the node has no parent, it is a root */
-
-    return (0); /* If it has a parent or is NULL, it is not a root */
+{   /* If the node has no parent, it is a root */
+	if (node && node->parent == NULL)
+		return (1); 
+	/* If it has a parent or is NULL, it is not a root */
+	return (0);
 }
